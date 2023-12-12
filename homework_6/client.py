97bc885b0a00dd8a4339d9517150a9ee5c9dbab9
yakdd <yakdd@yandex.ru>
2023-12-12 20:45:14 +0300

    homework_6

---
+++ homework_6/client.py
@@ -0,0 +1,32 @@
+import socket
+import threading
+
+
+nickname = input("Choose your nickname: ")
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 55555))
+
+def receive():
+    while True:
+        try:
+            message = client.recv(1024).decode('utf-8')
+            if message == 'NICK':
+                client.send(nickname.encode('utf-8'))
+            else:
+                print(message)
+        except:
+            print("An error occured!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = '{}: {}'.format(nickname, input(''))
+        client.send(message.encode('utf-8'))
+
+
+receive_thread = threading.Thread(target=receive)
+receive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
